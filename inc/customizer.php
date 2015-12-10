d6d039937003e71141c415248fd3945104c00764
Rogger Matamoros <roggmatz@gmail.com>
2015-12-10 17:28:29 -0500

    Initial Commit
    Added the default, untouched, starter files the Underscore theme has.

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Rog1 Theme Customizer.
+ *
+ * @package Rog1
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function rog1_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'rog1_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function rog1_customize_preview_js() {
+	wp_enqueue_script( 'rog1_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+}
+add_action( 'customize_preview_init', 'rog1_customize_preview_js' );
